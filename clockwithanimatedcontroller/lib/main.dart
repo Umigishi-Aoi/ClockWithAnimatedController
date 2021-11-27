d4b7ccbe2030f153c723980761c4ae7d818ea350
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-11-27 15:30:37 +0900

    add AnimatedController, complete.

--- clockwithanimatedcontroller/lib/main.dart
+++ clockwithanimatedcontroller/lib/main.dart
@@ -28,17 +28,42 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final duration = const Duration(seconds: 1);
+  late AnimationController _ctrl;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  bool _flag = true;
+
+  static const double _widthRatio = 0.7;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _ctrl = AnimationController(duration: duration, vsync: this)
+      ..addListener(() => setState(() {}))
+      ..forward()
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _ctrl.reverse();
+          _flag = false;
+        } else if (status == AnimationStatus.dismissed) {
+          _ctrl.forward();
+          _flag = true;
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * _widthRatio;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -47,20 +72,24 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            if (_flag)
+              SizedBox(
+                width: width,
+                child: Image.asset(
+                  "images/20211127_flag_red.png",
+                  fit: BoxFit.cover,
+                ),
+              )
+            else
+              SizedBox(
+                width: width,
+                child: Image.asset(
+                  "images/20211127_flag_white.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
